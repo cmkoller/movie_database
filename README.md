69c20fbaf188ce8dd7ec45aeefbafe10e5fac583
cmkoller <cmkoller01@gmail.com>
2014-11-20 12:18:52 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a Sinatra project for displaying movie information as read from a CVS file.
+
+The home page displays a list of movies in alphabetical order, broken into pages.
+
+The user can use the home page search box to search for a particular phrase, and movies containing that phrase in the title or description will be returned.
+
+Clicking a movie title will bring you to a page with more details on that movie.
---
+++ server.rb
@@ -0,0 +1,53 @@
+require 'sinatra'
+require 'CSV'
+
+# Takes in CSV, returns hash
+def process_movie_data
+  unprocessed = CSV.read("movies.csv")
+  processed = {}
+  unprocessed.each do |movie_string|
+    processed[movie_string[0]] = movie_string[1..-1]
+  end
+
+  processed
+end
+
+# Takes in hash, returns sorted array of key:value pairs
+def sort_by_title(movie_hash)
+  movie_hash.sort_by{|id, data| data[0]}
+end
+
+# Redirect homepage to /movies page
+get '/' do
+  redirect '/movies?:page=1'
+end
+
+# Display list of movies
+get '/movies' do
+  @query = params[:query]
+  if params[:page]
+    @page = params[:page].to_i
+  else
+    @page = 1
+  end
+  @movies_array = sort_by_title(process_movie_data)
+  @num_pages = (@movies_array.length / 20.0).ceil
+
+  @start_over_button = false
+  if @query
+    @query = @query.downcase
+    @movies_array = @movies_array.select do |x|
+      x[1][0].downcase.include?(@query) || x[1][1].downcase.include?(@query)
+    end
+    @start_over_button = true
+  end
+  erb :index
+end
+
+# Display individual movie
+get '/movies/:id' do
+  @movie_id = params[:id]
+  @movie_data = process_movie_data[@movie_id]
+
+  erb :movie_page
+end
